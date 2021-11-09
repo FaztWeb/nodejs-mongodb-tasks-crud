dd6bf6f0dbe0162df78a67b003e12346dc0804c5
fazttech <fazt@faztweb.com>
2021-11-09 12:04:21 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import exphbs from "express-handlebars";
+import indexRoutes from "./routes/index.routes";
+import path from "path";
+import morgan from "morgan";
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+
+app.engine(
+  ".hbs",
+  exphbs({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use(indexRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+export default app;
